d3ce718d833863d1cb7afc1ba46baf28a68bd7c4
Rafael <rg.gallegostapia@gmail.com>
2024-08-09 16:20:50 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//Zona validacion de formulario
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+
+        }, false)
+    })
+})()
+
+
+// zona de variable
+let animacion = document.querySelector(".inicio");
+let skills = document.querySelectorAll(".tecIcon");
+let title = document.querySelector(".skill-text");
+let bar = document.querySelector(".progress-bar");
+
+let skillSettings = {
+    "HTML": { width: "80%", background: "linear-gradient(0deg, #e44d26, #f16529, #f4f4f4)" },
+    "CSS": { width: "60%", background: "linear-gradient(0deg, #0478be, #009fe4, #f4f4f4)" },
+    "Javascript": { width: "50%", background: "linear-gradient(0deg, #f7df1e, #0a0a0a)" },
+    "React": { width: "50%", background: "linear-gradient(0deg, #0562b9, #61dafb)" },
+    "Bootstrap": { width: "60%", background: "linear-gradient(0deg, #563d7c, #ffffff)" },
+    "Git": { width: "60%", background: "linear-gradient(0deg, #f1502f, #ffffff)" },
+    "GitHub": { width: "60%", background: "linear-gradient(0deg, #d3d3d3, #404040, #808080)" }
+};
+
+
+skills.forEach(index => {
+    index.addEventListener("click", () => {
+        let alt = index.getAttribute("alt")
+        title.textContent = index.getAttribute("alt");
+        if (skillSettings[alt]) {
+            bar.style.transition = "width 0.5s ease-in-out";
+            bar.style.width = skillSettings[alt].width;
+            bar.style.background = skillSettings[alt].background;
+        }
+    });
+
+    index.addEventListener("mouseout", () => {
+        title.textContent = "Skills";
+        bar.style.width = "0%";
+    });
+});
+
+
+
